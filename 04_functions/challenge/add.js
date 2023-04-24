628166e8e11370ca7364d08005c0830459c2d269
djohn06 <danielajohn1406@gmail.com>
2023-04-24 16:02:25 +0100

    Ph1.4 | Created add & multiply func. < Chal. done

---
+++ 04_functions/challenge/add.js
@@ -0,0 +1,11 @@
+// Question 1:
+const add = (a, b) => {
+    return a + b;
+}
+module.exports = add;
+
+// Question 3:
+// To both add and multiply together, use the add function as a parameter
+// within the multiply function:
+
+// multiply(2, add(4,4));
---
+++ 04_functions/challenge/multiply.js
@@ -0,0 +1,11 @@
+// Question 2:
+const multiply = (a, b) => {
+    return (a * b);
+}
+module.exports = multiply;
+
+// Question 3:
+// To both add and multiply together, use the add function as a parameter
+// within the multiply function:
+
+// multiply(2, add(4,4));
