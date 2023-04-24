825462f89735407f10e7c130a826c00b5de3fcbf
djohn06 <danielajohn1406@gmail.com>
2023-04-24 12:23:24 +0100

    Ph1.2 | Greeting message < Exercise done

---
+++ 02_running_files/exercise/hello.js
@@ -0,0 +1,5 @@
+console.log("Hello, it's a lovely day today!"); // Remember the semicolon at the end!
+
+// If in directory 'w6_javascript_fundamentals', input into console:
+// 'node ./02_running_files/exercise/hello.js'
+// to print this string in the console.
