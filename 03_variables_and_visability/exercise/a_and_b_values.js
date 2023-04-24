a627a496fa30c93490b590167582e0ac739d4914
djohn06 <danielajohn1406@gmail.com>
2023-04-24 14:03:09 +0100

    Ph1.3 | Multiplication and addition < Ex. Complete

---
+++ 03_variables_and_visability/exercise/a_and_b_values.js
@@ -0,0 +1,10 @@
+// Set values given for a and b.
+a = 4;
+b = 6;
+
+// Return in the terminal the addition and multiplication of these values.
+// Addition:
+console.log(a + b);
+
+// Multiplication:
+console.log(a * b);
