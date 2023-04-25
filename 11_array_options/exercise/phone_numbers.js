a84bb1dc599b6e776c420081bf9f7d9ed8d57ce7
djohn06 <danielajohn1406@gmail.com>
2023-04-25 18:08:41 +0100

    Ph1.11 | Numbers filtered good < Ex01 complete

---
+++ 11_array_options/exercise/phone_numbers.js
@@ -0,0 +1,20 @@
+const numbers = [
+    '1763687364',
+    '4763687363',
+    '7867867862',
+    'aaaaaaaabbbbbbbcccccdddddddd' // this element should be filtered
+];
+
+const checkLength = (phone) => {
+    if (phone.length <= 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const filterLongNumber = (numbers) => {
+  return numbers.filter(checkLength);
+};
+
+console.log(filterLongNumber(numbers));
